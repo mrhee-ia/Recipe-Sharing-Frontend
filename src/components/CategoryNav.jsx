6c3a29cbaf6fbe82189b291294391adda6a87b7a
mrhee-ia <mcrispasague@gmail.com>
2024-08-24 15:32:59 +0800

    Category Navigation - css

--- src/components/CategoryNav.jsx
+++ src/components/CategoryNav.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './components.css'
 
 const categories = [
@@ -24,16 +25,22 @@ const categories = [
 ];
 
 const CategoryNavBar = ( {onCategorySelect} ) => {
+
+  const categoryClass = ({ isActive }) =>
+    isActive
+      ? 'text-sm px-4 py-2 rounded-3xl border-2 border-green-500 bg-white text-black hover:bg-green-500 hover:border-green-500 hover:text-white focus:outline-none'
+      : 'text-sm px-4 py-2 rounded-3xl border-2 border-green-500 bg-green-500 text-white focus:outline-none'
+
   return (
     <div className="w-full h-15 overflow-x-auto whitespace-nowrap py-2 scrollbar-hide">
       <div className="inline-flex space-x-2 px-4">
         {categories.sort().map((category, index) => (
-          <button
+          <NavLink
             key={index}
-            onClick={() => onCategorySelect(category)}
-            className="px-4 py-2 bg-white text-indigo rounded-3xl hover:bg-indigo-600 focus:outline-none">
-            <span className='text-sm'>{category}</span>
-          </button>
+            to='/'
+            className={categoryClass}>
+            {category}
+          </NavLink>
         ))}
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,15 @@
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <aside className="w-1/5 py-2">
         <nav className="space-y-4">
-            <Link to="#" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">User Profile</Link>
-            <Link to="#" className="block py-2 px-3 bg-blue-500 text-white text-white">Home</Link>
-            <Link to="my-recipes" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">My Recipes</Link>
-            <Link to="#" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">Liked Recipes</Link>
-            <Link to="#" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">Notifications</Link>
-            <Link to="#" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">Settings</Link>
+            <NavLink to="user-profile" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">User Profile</NavLink>
+            <NavLink to="/" className="block py-2 px-3 bg-blue-500 text-white text-white">Home</NavLink>
+            <NavLink to="my-recipes" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">My Recipes</NavLink>
+            <NavLink to="liked-recipes" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">Liked Recipes</NavLink>
+            <NavLink to="notification" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">Notifications</NavLink>
+            <NavLink to="settings" className="block py-2 px-3 hover:bg-blue-500 hover:text-white text-white">Settings</NavLink>
         </nav>
     </aside>
   )
